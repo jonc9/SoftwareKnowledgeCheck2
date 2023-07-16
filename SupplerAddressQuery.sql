d2d29612d46852c70fd9bdd19dac1ffaadf4b150
Jon Cundiff <jmc9.1987@gmail.com>
2023-07-16 16:42:52 -0400

    deleting incorrect file name

--- SupplerAddressQuery.sql
+++
@@ -1,4 +0,0 @@
-SELECT Products.ProductID, Products.ProductName AS Product, Suppliers.SupplierID, Suppliers.SupplierName AS Supplier, Suppliers.Address
-FROM Products
-INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
-WHERE ProductID = 40;
